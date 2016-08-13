a83b8a4662fa3ae84c4f7cd4d69519089b23b065
Mike Stratton <mike@mikestratton.net>
2016-08-13 04:45:21 -0400

    form

---
+++ geo_form.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['field1'])) {
+    $data = $_POST['field1'] . "\n";
+    $ret = file_put_contents('maps/geo_form.json', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file <br />";
+		echo "View the data here: <a href=\"maps/geo_form.json\">geo_form.json</a>";
+    }
+}
+else {
+   die('no post data to process');
+}
