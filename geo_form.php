eb6bddb43d77455236898310b94e3750e8efea45
Mike Stratton <mike@mikestratton.net>
2016-10-21 06:34:15 -0400

    php posting to textaree but dom not registering in map

--- geo_form.php
+++ geo_form.php
@@ -1,17 +1,13 @@
 <?php
 if(isset($_POST['geofield1'])) {
     $data = $_POST['geofield1'] . "\n";
-    $ret = file_put_contents('maps/geo_form.json', $data, FILE_APPEND | LOCK_EX);
+    $ret = file_put_contents('maps/geo_form.json', $data);
     if($ret === false) {
         die('There was an error writing this file');
     }
     else {
         echo "$ret bytes written to file <br />";
-<<<<<<< HEAD
-		echo "View the data here: <a href=\"create_json_file2.php\">Saved Map</a>";
-=======
-		echo "View the data here: <a href=\"maps/geo_form.json\">geo_form.json</a>";
->>>>>>> parent of 8d9e591... Merge pull request #6 from ykoeu/feature
+        echo "View the data here: <a href=\"create_json_file.php\">Saved Map</a>";
     }
 }
 else {
