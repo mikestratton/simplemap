71242d52c1efaf61cd7bd7339f01e2bea0fad34d
Mike Stratton <mike@mikestratton.net>
2016-10-21 05:14:41 -0400

    fixing merge

--- geo_form.php
+++ geo_form.php
@@ -7,7 +7,11 @@ if(isset($_POST['geofield1'])) {
     }
     else {
         echo "$ret bytes written to file <br />";
+<<<<<<< HEAD
 		echo "View the data here: <a href=\"create_json_file2.php\">Saved Map</a>";
+=======
+		echo "View the data here: <a href=\"maps/geo_form.json\">geo_form.json</a>";
+>>>>>>> parent of 8d9e591... Merge pull request #6 from ykoeu/feature
     }
 }
 else {
