ed717e0f62d8bf46f5ae3f4d6e037441a68d7a5d
Mike Stratton <mike@mikestratton.net>
2016-11-04 02:23:59 -0400

    reverted back to v0.15.1 and v0.15.2

--- v0.16.php
+++ v0.16.php
@@ -215,7 +215,7 @@
   </head>
   <body>
   <h1>Upload Image</h1>
-  <form method="post" enctype="multipart/form-data">
+  <form action="" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
