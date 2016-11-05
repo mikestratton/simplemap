6f15c5a74ff3ce596fa93e6e0f4da7dde2d9f443
ykoeu <kochug.eugene@gmail.com>
2016-11-05 08:47:49 +0200

    images editor on map overlay feature v16

---
+++ yev_v0.16.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TSSP Simple Map</title>
+   <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+  <body style="margin:20px 40px">
+<h1>Upload Image</h1>
+<form action="yev_v0.16.1.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+  </body>
+</html>	
