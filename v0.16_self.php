71b1730835347c0fa79826263209d5c3b9b3db5f
Mike Stratton <mike@mikestratton.net>
2016-11-12 16:59:39 -0500

    update version 0.16, security testing requirement added

---
+++ v0.16_self.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TSSP Simple Map</title>
+   <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+  <body style="margin:20px 40px">
+<h1>Upload Image</h1>
+<form name="upload1" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+   
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+  </body>
+</html>	
