7d02bd199be1fbf2099cc3e828712877878f6328
mikestratton <mike@mikestratton.net>
2017-03-13 15:12:06 -0400

    css update and php removal in index.php

--- demo/index.php
+++ demo/index.php
@@ -1,56 +1,3 @@
-<?php
-$target_dir = "uploads/";
-$target_file = null;
-/* @version 0.28 bugfix code below: php undefined index notices */
-if (isset($_FILES["fileToUpload"])){
-	$imageFileName = (isset($_FILES["fileToUpload"]["name"])) ? $_FILES["fileToUpload"]["name"] : null;
-	$imageFileSize = (isset($_FILES["fileToUpload"]["size"])) ? $_FILES["fileToUpload"]["size"] : null;
-	$imageFileTmpName = (isset($_FILES["fileToUpload"]["tmp_name"])) ? $_FILES["fileToUpload"]["tmp_name"] : null;
-	$target_file = $target_dir . basename($imageFileName);
-	$uploadOk = 1;
-	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-	// Check if image file is a actual image or fake image
-	if(isset($_POST["submit"])) {
-		$check = getimagesize($imageFileTmpName);
-		if($check !== false) {
-			// echo "File is an image - " . $check["mime"] . ".";
-			$uploadOk = 1;
-		} else {
-			echo "File is not an image.";
-			$uploadOk = 0;
-		}
-	}
-	// Check if file already exists
-	if (file_exists($target_file)) {
-		//echo "Sorry, file already exists.";
-		$uploadOk = 0;
-	}
-	// Check file size
-	if ($imageFileSize > 50000000) {
-		echo "Sorry, your file is too large.";
-		$uploadOk = 0;
-	}
-	// Allow certain file formats when image is provided
-	if ($target_file != null){
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-			$uploadOk = 0;
-		}
-	}
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-		//echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-	} else {
-		if (move_uploaded_file($imageFileTmpName, $target_file)) {
-		//echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		} else {
-			// echo "Sorry, there was an error uploading your file.";
-		}
-	}
-}
-?>
-
 <!DOCTYPE html>
 <?php $timestamp = date("YmdHis");?>
 <html>
@@ -61,7 +8,7 @@ if (isset($_FILES["fileToUpload"])){
 	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.3/themes/base/jquery-ui.css" type="text/css"/>
 	<link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
 	<link rel="stylesheet" href="styles/googlemaps.css" type="text/css"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <meta charset="utf-8">
 	<link rel="stylesheet" href="styles/style.css" type="text/css"/>
     <title>Maps & GIS Web Applications with Google Maps JavaScript API</title>
@@ -123,7 +70,7 @@ if (isset($_FILES["fileToUpload"])){
         </div>
 		</div>
     </div>
-    <div><button id="delete-button">Delete Selected Shape</button></div>
+    <div><button id="delete-button">Delete Selected GeoJSON</button></div>
     <div id="map-container">
       <div id="map-holder" style="position: relative; overflow: hidden; background-color: rgb(229, 227, 223);"></div>
       <div id="drop-container" class=""></div>
