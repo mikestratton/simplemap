1bb17e97cc1b64e0ffc67e4c820dff7b94cc5ea5
mikestratton <mike@mikestratton.net>
2017-03-29 06:13:44 -0400

    responsive to mobile devices

--- demo-mobile/index.php
+++ demo-mobile/index.php
@@ -31,19 +31,33 @@
 				<div class="col-md-3">
 				<div class="logo">
 	  <p><a href="http://mikestratton.net"><img src="http://www.mikestratton.net/assets/text_logo_174x40.png"></a></p>
-	  <h3><a href="http://mikestratton.net">Geographical Information Systems for the Web<br />
-	  Built With Care Using Google Maps Javascript API</a></h3>
-	  <p><a href="http://mikestratton.net"><img src="uploads/globe_by_atlantis.png" width="80px" height="80px" title="Image courtesy of Atlantis https://openclipart.org/user-detail/atlantis"></a>
-	  </p>
+	  <p><a href="http://mikestratton.net">Geographical Information Systems<br />
+	  Built Using Google Maps Javascript API</a></p>
+	 
 	  </div>
 				</div>
 				<div class="col-md-3">
 				<div id="controls">
-								<h2 style="color:white;text-align:center;background:navy;border:double thick white;padding:4px;">Image Controls</h2>
-				<div id="opacity" class="slide-bg" data-wjs-element="edit"><span class="slabel">OPACITY</span></div>
-				<div id="rotate" class="slide-bg" data-wjs-element="edit"><span class="slabel">ROTATE</span></div>
-				<div id="height" class="slide-bg" data-wjs-element="hw"><span class="slabel">HEIGHT</span></div>
-				<div id="width" class="slide-bg" data-wjs-element="hw"><span class="slabel">WIDTH</span></div><br>
+								<p>Image Controls</p>
+				<div class="row">
+					<div class="col-md-6">
+						<div id="opacity" class="slide-bg" data-wjs-element="edit"><span class="slabel">OPACITY</span></div>
+					</div>
+					<div class="col-md-6">
+						<div id="rotate" class="slide-bg" data-wjs-element="edit"><span class="slabel">ROTATE</span></div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div id="height" class="slide-bg" data-wjs-element="hw"><span class="slabel">HEIGHT</span></div>
+					</div>
+					<div class="col-md-6">
+						<div id="width" class="slide-bg" data-wjs-element="hw"><span class="slabel">WIDTH</span></div>
+					</div>
+				</div>
+				
+				
+				
 			
 				<form action="final.php" method="post" enctype="multipart/form-data">
 					
@@ -66,7 +80,7 @@
 		 
         
         
-        <textarea name="geofield1" form="geoform" id="geojson-input" placeholder="Drag and drop GeoJSON onto the map or paste it here to begin editing." style="height: 611.5px;"></textarea>
+        <textarea name="geofield1" form="geoform" id="geojson-input" placeholder="Drag and drop GeoJSON onto the map or paste it here to begin editing." ></textarea>
       <div id="geojson-controls">
           <button onclick="document.getElementById('geojson-input').select();">Select All</button>
           <a id="download-link" href="data:;base64," download="geojson.json">
@@ -78,10 +92,10 @@
 				</div>
 				<div class="col-md-3">
 					<form class="input-form" id="input-form">
-		<p><table border="0">
-				<tr><td>Latitude:</td><td><input type="text" name="lat" id="lat-input" value="40.7116" /></td></tr>
-				<tr><td>Longitude:</td><td><input type="text" name="lon" id="lon-input" value="-74.0132" /></td></tr>
-				<tr><td>Zoom Level:</td><td><input type="text" name="zoom" id="zoom-input" value="18" /></td></tr>
+		<p><table style="margin-left:5%" border="0">
+				<tr><td>Latitude:&nbsp;&nbsp; </td><td style="text-align:left;"><input type="text" name="lat" id="lat-input" value="40.7116" /></td></tr>
+				<tr><td>Longitude:&nbsp;&nbsp; </td><td style="text-align:left;"><input type="text" name="lon" id="lon-input" value="-74.0132" /></td></tr>
+				<tr><td>Zoom Level:&nbsp;&nbsp; </td><td style="text-align:left;"><input type="text" name="zoom" id="zoom-input" value="18" /></td></tr>
 				<tr><td>&nbsp;</td>
 					<td style="text-align:right; padding:3px 12px">
 					<input type="submit" value="Center Map" />
@@ -92,18 +106,13 @@
 	</form>	
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-12">
-				</div>
-			</div>
+			
 			
 		</div>
 	</div>
 </div>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
+
   
   
   
@@ -125,7 +134,7 @@
 <a href="https://github.com/mikestratton/simplemap"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"></a>
 
     
-    <div><button id="delete-button">Delete Selected GeoJSON</button></div>
+    <div><button id="delete-button">Delete</button></div>
     <div id="map-container">
       <div id="map-holder" style="position: relative; overflow: hidden; background-color: rgb(229, 227, 223);"></div>
       <div id="drop-container" class=""></div>
@@ -162,6 +171,9 @@
 
 
 	</script>
+	    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
  </body>
 </html>
 
